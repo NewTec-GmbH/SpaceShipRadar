f4d7764d6649361a43e3d23085d39570b651e694
Marc <marc.trosch@newtec.de>
2025-02-04 13:10:22 +0100

    fixed random color

--- src/space_ship_radar/utils/helper.py
+++ src/space_ship_radar/utils/helper.py
@@ -34,15 +34,15 @@ class ColorGenerator:
         new_color: np.ndarray = np.random.randint(50, 200, (1, 3))[0]
 
         # Check for maximum distance from previous colors
-        i: int = 0
+        i: int = -1
         while self.previous_colors:
+            i += 1
             distances = [np.linalg.norm(new_color - np.array(color))
                          for color in self.previous_colors]
-            if min(distances) < 100 or i > 3:
+            if min(distances) < 100 and i < 3:
                 new_color = np.random.randint(0, 200, (1, 3))[
                     0]  # Regenerate if too close
             else:
-                i += 1
                 break
 
         # Store the new color
