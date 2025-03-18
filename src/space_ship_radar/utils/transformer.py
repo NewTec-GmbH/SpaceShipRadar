73da7bbdc87b578d4f629927a9d530d734e94937
Marc <marc.trosch@newtec.de>
2025-03-18 15:02:23 +0100

    even more comments

--- src/space_ship_radar/utils/transformer.py
+++ src/space_ship_radar/utils/transformer.py
@@ -40,12 +40,16 @@ class Transformer:
 
         top_l, top_r, bottom_r, bottom_l = corners
 
+        # Determine width of new image which is the max distance between
+        # (bottom right and bottom left) or (top right and top left) x-coordinates
         width_a = np.sqrt(((bottom_r[0] - bottom_l[0])
                            ** 2) + ((bottom_r[1] - bottom_l[1]) ** 2))
         width_b = np.sqrt(((top_r[0] - top_l[0]) ** 2) +
                           ((top_r[1] - top_l[1]) ** 2))
         width = max(int(width_a), int(width_b))
 
+        # Determine height of new image which is the max distance between
+        # (top right and bottom right) or (top left and bottom left) y-coordinates
         height_a = np.sqrt(((top_r[0] - bottom_r[0]) ** 2) +
                            ((top_r[1] - bottom_r[1]) ** 2))
         height_b = np.sqrt(((top_l[0] - bottom_l[0]) ** 2) +
