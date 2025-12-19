e00d061ed76bdea363198c179d309deca47a29d4
Tobias Haeckel <tobias.haeckel@newtec.de>
2025-12-19 16:53:52 +0100

    test

--- tests/test_object_finder.py
+++ tests/test_object_finder.py
@@ -47,7 +47,7 @@ def test_object_finder():
     assert markers[3].speed_y == 0
     assert markers[3].angle == 1570.8
 
-    assert markers[7].position_x == 910
+    assert markers[7].position_x == 1012.0
     assert markers[7].position_y == 147
     assert markers[7].speed_x == 0
     assert markers[7].speed_y == 0
